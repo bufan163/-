2aae43dae74f92b622a0a2341d3d4c97c56f4ca7
Abufan <Inno_abufan@git.cloud.tencent.com>
2020-04-11 17:08:05 +0800

    装饰器模式

---
+++ 装饰器模式/InputStream.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace 装饰器模式
+{
+
+    public abstract class InputStream
+    {
+        //...
+        public int read(List<byte> b)
+        {
+            return read(b, 0, b.Count);
+        }
+
+        public int read(List<byte> b, int off, int len)
+        {
+            return 0;
+        }
+
+        public long skip(long n)
+        {
+            return 0L;
+        }
+
+        public int available()
+        {
+            return 0;
+        }
+
+        public void close() { }
+
+        public async void mark(int readlimit) { }
+
+        public async void reset()
+        {
+            throw new IOException("mark/reset not supported");
+        }
+
+        public bool markSupported()
+        {
+            return false;
+        }
+    }
+
+    public class FileInputStream : InputStream
+    {
+        protected volatile InputStream inputStream;
+        public FileInputStream(string inputStream)
+        {
+             //this.inputStream;
+        }
+    }
+    public class FilterInputStream : InputStream
+    {
+        protected volatile InputStream inputStream;
+
+        public FilterInputStream(InputStream inputStream)
+        {
+            this.inputStream = inputStream;
+        }
+    }
+
+    public class BufferedInputStream : FilterInputStream
+    {
+        protected volatile InputStream input;
+
+        public BufferedInputStream(InputStream input) : base(input)
+        {
+            this.input = input;
+        }
+        //...实现基于缓存的读数据接口...  
+    }
+
+    public class DataInputStream : FilterInputStream
+    {
+        protected volatile InputStream input;
+
+        public DataInputStream(InputStream input) : base(input)
+        {
+            this.input = input;
+        }
+
+        //...实现读取基本类型数据的接口
+    }
+}
---
+++ 装饰器模式/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace 装饰器模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputStream inputStream= new FileInputStream("");
+            InputStream bin = new BufferedInputStream(inputStream);
+            DataInputStream dataInputStream =new DataInputStream(bin);
+            dataInputStream.read(new System.Collections.Generic.List<byte>() { });
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

d9d7231b38404ed8e59ca0a10f005a73111763f3
Abufan <Inno_abufan@git.cloud.tencent.com>
2020-04-11 15:18:50 +0800

    桥接模式

---
+++ 桥接模式/MsgSend.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 桥接模式
+{
+
+    public interface MsgSender
+    {
+        void send(string message);
+    }
+
+    public class TelephoneMsgSender : MsgSender
+    {
+        private List<string> telephones;
+
+        public TelephoneMsgSender(List<string> telephones)
+        {
+            this.telephones = telephones;
+        }
+        public void send(string message)
+        {
+            Console.WriteLine($"TelephoneMsgSender-Send:message{message}");
+        }
+
+    }
+
+    public class EmailMsgSender : MsgSender
+    {
+        // 与TelephoneMsgSender代码结构类似，所以省略...
+        public void send(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class WechatMsgSender : MsgSender
+    {
+        // 与TelephoneMsgSender代码结构类似，所以省略...
+        public void send(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public abstract class Notification
+    {
+        protected MsgSender msgSender;
+
+        public Notification(MsgSender msgSender)
+        {
+            this.msgSender = msgSender;
+        }
+
+        public abstract void notify(string message);
+    }
+
+    public class SevereNotification : Notification
+    {
+        public SevereNotification(MsgSender msgSender) : base(msgSender)
+        {
+            Console.WriteLine("对象初始化完成");
+            //super(msgSender);
+        }
+
+
+        public override void notify(string message)
+        {
+            Console.WriteLine($"调用send方法,传入msg,notify:{message}");
+            msgSender.send(message);
+        }
+    }
+
+    public class UrgencyNotification : Notification
+    {
+        public UrgencyNotification(MsgSender msgSender) : base(msgSender)
+        {
+
+        }
+        // 与SevereNotification代码结构类似，所以省略...
+        public override void notify(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class NormalNotification : Notification
+    {
+        public NormalNotification(MsgSender msgSender) : base(msgSender)
+        {
+
+        }
+        // 与SevereNotification代码结构类似，所以省略...
+        public override void notify(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class TrivialNotification : Notification
+    {
+        public TrivialNotification(MsgSender msgSender) : base(msgSender)
+        {
+
+        }
+        // 与SevereNotification代码结构类似，所以省略...
+        public override void notify(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 桥接模式/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 桥接模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //方法是从最内层往外层调用的，首先通过TelephoneMsgSender构造函数初始化参数，构建MsgSender对象
+            SevereNotification severeNotification = new SevereNotification(new TelephoneMsgSender(new List<string> { "1234" }));
+            //把参数123，传到SevereNotification，然后再调用TelephoneMsgSender的 send方法
+            severeNotification.notify("123");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}

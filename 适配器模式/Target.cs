49e2f8aae4a12ca0dfaf9007ee67610376669080
Abufan <Inno_abufan@git.cloud.tencent.com>
2020-04-11 17:24:30 +0800

    适配器模式

---
+++ 适配器模式/Target.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 适配器模式
+{
+
+    // 类适配器: 基于继承
+    public interface ITarget
+    {
+        void f1();
+        void f2();
+        void fc();
+    }
+
+    public class Adaptee
+    {
+        public void fa()
+        { //... 
+        }
+        public void fb()
+        { //... 
+        }
+        public void fc()
+        { //... 
+        }
+    }
+
+    public class Adaptor : Adaptee, ITarget
+    {
+        public void f1()
+        {
+        }
+
+        public void f2()
+        {
+            //...重新实现f2()...
+        }
+        // 这里fc()不需要实现，直接继承自Adaptee，这是跟对象适配器最大的不同点
+    }
+
+}
---
+++ 适配器模式/Target2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 适配器模式
+{
+    // 对象适配器：基于组合
+    public interface ITarget2
+    {
+        void f1();
+        void f2();
+        void fc();
+    }
+
+    public class Adaptee2
+    {
+        public void fa()
+        { //... 
+        }
+        public void fb()
+        { //...
+        }
+        public void fc()
+        { //... 
+        }
+    }
+
+    public class Adaptor2 : ITarget2
+    {
+        private Adaptee2 adaptee;
+        public Adaptor2(Adaptee2 adaptee)
+        {
+            this.adaptee = adaptee;
+        }
+        public void f1()
+        {
+            adaptee.fa(); //委托给Adaptee
+        }
+        public void f2()
+        {
+            //...重新实现f2()...
+        }
+        public void fc()
+        {
+            adaptee.fc();
+        }
+    }
+}

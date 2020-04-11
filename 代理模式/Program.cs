de9c84b095c700ddb4208b724f735e403150314b
Abufan <Inno_abufan@git.cloud.tencent.com>
2020-04-11 14:53:00 +0800

    代理模式

---
+++ 代理模式/Program.cs
@@ -0,0 +1,20 @@
+﻿using Castle.DynamicProxy;
+using System;
+
+namespace 代理模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IUserController userController = new UserControllerProxy(new UserController());
+            userController.login("123","456");
+            Console.ReadKey();
+            // 代理插件
+            ProxyGenerator proxyGenerator = new ProxyGenerator();
+
+
+        }
+    }
+}
---
+++ 代理模式/UserProxy.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 代理模式
+{
+    public class UserVo
+    {
+
+    }
+    public class RequestInfo
+    {
+        public RequestInfo(string method, long responsTime, long startTimestamp)
+        {
+            Console.WriteLine($"method:{method},responsTime:{responsTime},startTimestamp:{startTimestamp}");
+        }
+    }
+    public class MetricsCollector
+    {
+        public void recordRequest(RequestInfo requestInfo)
+        {
+
+        }
+    }
+    public interface IUserController
+    {
+        public abstract UserVo login(string telephone, string password);
+        public abstract UserVo register(string telephone, string password);
+    }
+
+    public class UserController : IUserController
+    {
+        //...省略其他属性和方法...
+
+        public UserVo login(string telephone, string password)
+        {
+            Console.WriteLine("UserController-login");
+            //...省略login逻辑...
+            //...返回UserVo数据...
+            return new UserVo { };
+        }
+
+
+        public UserVo register(string telephone, string password)
+        {
+            //...省略register逻辑...
+            //...返回UserVo数据...
+            return new UserVo { };
+        }
+    }
+
+    public class UserControllerProxy : IUserController
+    {
+        private MetricsCollector metricsCollector;
+        private UserController userController;
+
+        public UserControllerProxy(UserController userController)
+        {
+            this.userController = userController;
+            this.metricsCollector = new MetricsCollector();
+        }
+
+
+        public UserVo login(string telephone, string password)
+        {
+            Console.WriteLine("UserControllerProxy-login");
+            long startTimestamp = DateTime.Now.Millisecond;
+            // 委托
+            UserVo userVo = userController.login(telephone, password);
+            long endTimeStamp = DateTime.Now.Millisecond;
+            long responseTime = endTimeStamp - startTimestamp;
+            RequestInfo requestInfo = new RequestInfo("login", responseTime, startTimestamp);
+            metricsCollector.recordRequest(requestInfo);
+
+            return userVo;
+        }
+
+
+        public UserVo register(string telephone, string password)
+        {
+            long startTimestamp = DateTime.Now.Millisecond;
+            UserVo userVo = userController.register(telephone, password);
+            long endTimeStamp = DateTime.Now.Millisecond;
+            long responseTime = endTimeStamp - startTimestamp;
+            RequestInfo requestInfo = new RequestInfo("register", responseTime, startTimestamp);
+            metricsCollector.recordRequest(requestInfo);
+            return userVo;
+        }
+
+    }
+
+
+}
+

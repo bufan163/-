ce8b5cacdbb84220ac01eab4b1d41dc07fb5673f
abindef@gmail.com <abindef@gmail.com>
2020-05-06 22:03:54 +0800

    组合模式

---
+++ 组合模式/FileSystemNode.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace 组合模式
+{
+
+    public class FileSystemNode
+    {
+        private String path;
+        private bool isFile;
+        private List<FileSystemNode> subNodes = new List<FileSystemNode>();
+
+        public FileSystemNode(String path, bool isFile)
+        {
+            this.path = path;
+            this.isFile = isFile;
+        }
+
+        public int countNumOfFiles()
+        {
+            if (isFile) { return 1; }
+            int numOfFiles = 0;
+            foreach (FileSystemNode fileOrDir in subNodes)
+            {
+                numOfFiles += fileOrDir.countNumOfFiles();
+            }
+            return numOfFiles;
+        }
+
+        public long countSizeOfFiles()
+        {
+            if (isFile)
+            {
+                if (!File.Exists(path)) return 0;
+                return File.ReadAllText(path).Length;
+            }
+            long sizeofFiles = 0;
+            foreach (FileSystemNode fileOrDir in subNodes)
+            {
+                sizeofFiles += fileOrDir.countSizeOfFiles();
+            }
+            return sizeofFiles;
+        }
+
+        public String getPath()
+        {
+            return path;
+        }
+
+        public void addSubNode(FileSystemNode fileOrDir)
+        {
+            subNodes.Add(fileOrDir);
+        }
+
+        public void removeSubNode(FileSystemNode fileOrDir)
+        {
+            int size = subNodes.Count;
+            int i = 0;
+            for (; i < size; ++i)
+            {
+                if (subNodes[i].getPath().Equals(fileOrDir.getPath()))
+                {
+                    break;
+                }
+            }
+            if (i < size)
+            {
+                subNodes.Remove(i);
+            }
+        }
+    }
+}
---
+++ 组合模式/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 组合模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

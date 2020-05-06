6cbe9fbcda898edac00210f8b8806ed38b5474ea
abindef@gmail.com <abindef@gmail.com>
2020-05-06 22:03:28 +0800

    门面模式

---
+++ 门面模式/Facade.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 门面模式
+{
+    class Facade : IA, IB, IFacade
+    {
+        public string FuncA()
+        {
+            Console.WriteLine("Facade FuncA");
+            return "A";
+        }
+
+        public string FuncB()
+        {
+            Console.WriteLine("Facade FuncB");
+            return "B";
+        }
+        public string FuncC()
+        {
+            Console.WriteLine("Facade FuncC");
+            return "C";
+        }
+        public string FuncD()
+        {
+            Console.WriteLine("门面模式调用开始");
+            IA a = new A();
+            IB b = new B();
+            a.FuncA();
+            b.FuncB();
+            Console.WriteLine("Facade FuncD");
+            return "C";
+        }
+    }
+    interface IFacade
+    {
+        string FuncC();
+    }
+    interface IA
+    {
+        string FuncA();
+    }
+    interface IB
+    {
+        string FuncB();
+    }
+    class A : IA
+    {
+        public string FuncA()
+        {
+            Console.WriteLine(" FuncA");
+            return "A";
+        }
+    }
+    class B : IB
+    {
+        public string FuncB()
+        {
+            Console.WriteLine(" FuncB");
+            return "B";
+        }
+    }
+}
---
+++ 门面模式/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace 门面模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IFacade c = new Facade();
+            c.FuncC();
+            IA a = new A();
+            a.FuncA();
+            Facade cc = new Facade();
+            cc.FuncA();
+            cc.FuncB();
+            cc.FuncC();
+            cc.FuncD();
+            Console.ReadKey();
+        }
+    }
+}

525319baf470a382f653203475a85edb276afda4
Abufan <Inno_abufan@git.cloud.tencent.com>
2020-05-09 16:22:05 +0800

    职责链模式

---
+++ 职责链模式/Handler.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 职责链模式
+{
+
+    public abstract class Handler
+    {
+        protected Handler successor = null;
+
+        public void setSuccessor(Handler successor)
+        {
+            this.successor = successor;
+            Console.WriteLine($"[Handler赋值]:{successor}");
+        }
+        public abstract void handle();
+    }
+
+    public class HandlerA : Handler
+    {
+
+        public override void handle()
+        {
+            bool handled = false;
+            if (!handled && successor != null)
+            {
+                Console.WriteLine($"handled:{handled}");
+                Console.WriteLine($"successor:{successor}");
+                successor.handle();
+            }
+        }
+    }
+
+    public class HandlerB : Handler
+    {
+
+        public override void handle()
+        {
+            bool handled = false;
+            if (!handled && successor != null)
+            {
+                Console.WriteLine($"handled:{handled}");
+                Console.WriteLine($"successor:{successor}");
+                successor.handle();
+            }
+        }
+    }
+    public class HandlerC : Handler
+    {
+
+        public override void handle()
+        {
+            bool handled = false;
+            if (!handled && successor != null)
+            {
+                Console.WriteLine($"handled:{handled}");
+                Console.WriteLine($"successor:{successor}");
+                successor.handle();
+            }
+        }
+    }
+    public class HandlerChain
+    {
+        private Handler head = null;
+        private Handler tail = null;
+
+        public void addHandler(Handler handler)
+        {
+           //设置handle
+            handler.setSuccessor(null);
+            Console.WriteLine($"head_set:{handler}");
+            
+            if (head == null)
+            {
+                head = handler;
+                tail = handler;
+                return;
+            }
+           //设置tail(尾巴)
+            tail.setSuccessor(handler);
+            tail = handler;
+            Console.WriteLine($"tail_set:{handler}");
+        }
+
+        public void handle()
+        {
+            if (head != null)
+            {
+                Console.WriteLine($"{head}");
+                head.handle();
+            }
+        }
+    }
+
+
+}
---
+++ 职责链模式/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace 职责链模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            HandlerChain chain = new HandlerChain();
+            chain.addHandler(new HandlerA());
+            chain.addHandler(new HandlerB());
+            chain.addHandler(new HandlerC());
+            chain.handle();
+            Console.ReadKey();
+        }
+    }
+}

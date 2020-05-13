b687770d13dd65df8b6419da268b86fc7ff66fd4
Abufan <Inno_abufan@git.cloud.tencent.com>
2020-05-13 16:15:25 +0800

    迭代器模式

---
+++ 迭代器模式/Iterator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 迭代器模式
+{
+
+    public class ArrayIterator<T> 
+    {
+        private int cursor;
+        private List<T> arrayList;
+
+        public ArrayIterator(List<T> arrayList)
+        {
+            this.cursor = 0;
+            this.arrayList = arrayList;
+        }
+        public bool hasNext()
+        {
+            return cursor != arrayList.Count; //注意这里，cursor在指向最后一个元素的时候，hasNext()仍旧返回true。
+        }
+        public void next()
+        {
+            cursor++;
+        }
+        public T currentItem()
+        {
+            if (cursor >= arrayList.Count)
+            {
+                throw new ArgumentNullException();
+            }
+            return arrayList[cursor];
+        }
+    }
+}
---
+++ 迭代器模式/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 迭代器模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<string> names = new List<string>();
+            names.Add("xzg");
+            names.Add("wang");
+            names.Add("zheng");
+
+            ArrayIterator<string> iterator = new ArrayIterator<string>(names);
+            while (iterator.hasNext())
+            {
+                Console.WriteLine(iterator.currentItem());
+                iterator.next();
+            }
+            Console.ReadKey();
+        }
+    }
+}

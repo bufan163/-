f79ec0a16f8ff650c72278324f9378568a5416d2
Abufan <Inno_abufan@git.cloud.tencent.com>
2020-05-13 17:07:36 +0800

    yield

--- 迭代器模式/Program.cs
+++ 迭代器模式/Program.cs
@@ -5,6 +5,14 @@ namespace 迭代器模式
 {
     class Program
     {
+
+        //一个返回类型为IEnumerable<int>，其中包含三个yield return
+        public static IEnumerable<int> enumerableFuc()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
@@ -19,6 +27,13 @@ namespace 迭代器模式
                 Console.WriteLine(iterator.currentItem());
                 iterator.next();
             }
+
+            #region yield 用法
+            foreach (int item in enumerableFuc())
+            {
+                Console.WriteLine($"yield :{item}");
+            }
+            #endregion
             Console.ReadKey();
         }
     }

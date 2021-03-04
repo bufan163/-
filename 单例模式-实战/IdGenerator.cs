ca31e63d92530d2d07923dee7c052ab0678b2de9
abindef <abindef@163.com>
2021-03-04 16:07:18 +0800

    Id生成器

---
+++ 单例模式-实战/IdGenerator.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.Services.CircuitBreaker;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 单例模式_实战
+{
+
+    public class IdGenerator
+    {
+
+        private AtomicLong id = new AtomicLong(0);
+        private static IdGenerator instance;
+        private IdGenerator() { }
+        private static readonly object padlock = new object();
+        public static IdGenerator getInstance()
+        {
+            if (instance == null)
+            {
+                lock (padlock)
+                { // 此处为类级别的锁
+                    if (instance == null)
+                    {
+                        instance = new IdGenerator();
+                    }
+                }
+            }
+            return instance;
+        }
+        public long getId()
+        {
+            return id.IncrementAndGet();
+        }
+    }
+}

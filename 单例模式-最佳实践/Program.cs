da5d084c1a18a2255cbd314449c9074d95776ead
abindef <abindef@163.com>
2021-03-04 16:14:27 +0800

    最佳实践

---
+++ 单例模式-最佳实践/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 单例模式_最佳实践
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

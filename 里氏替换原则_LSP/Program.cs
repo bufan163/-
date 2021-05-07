221d8ec2c59978f2426f521098e7068ba836163b
abindef <abindef@163.com>
2021-05-07 17:52:38 +0800

    增加里氏替换原则

---
+++ 里氏替换原则_LSP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+
+namespace 里氏替换原则_LSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 里式替换原则
+            Demo demo = new Demo();
+            HttpClient httpClient = new HttpClient();
+            String appId="";
+            String appToken="";
+            //SecurityTransporter在这里作为子类替换了方法中定义父类Transporter
+            demo.demoFunction(new SecurityTransporter(httpClient,appId,appToken));
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 里氏替换原则_LSP/Transporter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace 里氏替换原则_LSP
+{
+
+    public class Transporter
+    {
+        private HttpClient httpClient;
+
+        public Transporter(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public virtual HttpResponseMessage sendRequest(HttpRequestMessage request)
+        {
+            // ...use httpClient to send request
+            return new HttpResponseMessage();
+        }
+    }
+
+    public class SecurityTransporter : Transporter
+    {
+        private String appId;
+        private String appToken;
+
+        public SecurityTransporter(HttpClient httpClient, String appId, String appToken) : base(httpClient)
+        {
+            this.appId = appId;
+            this.appToken = appToken;
+        }
+
+
+        public override HttpResponseMessage sendRequest(HttpRequestMessage request)
+        {
+            if (!string.IsNullOrEmpty(appId) && !string.IsNullOrEmpty(appToken))
+            {
+                //处理appid 和 apptoken
+                //request.addPayload("app-id", appId);
+                //request.addPayload("app-token", appToken);
+            }
+            return base.sendRequest(request);
+        }
+    }
+
+    public class Demo
+    {
+        public void demoFunction(Transporter transporter)
+        {
+            HttpRequestMessage request = new HttpRequestMessage();
+            //...省略设置request中数据值的代码...
+            HttpResponseMessage response = transporter.sendRequest(request);
+            //...省略其他逻辑...
+        }
+    }
+}
